57061b527e98b59b104369b080119001a67d0bd1
naveen786pal <naveen786pal@gmail.com>
2024-05-24 12:46:10 +0530

    test2

--- src/main/java/com/demo/Hello.java
+++ src/main/java/com/demo/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		int a =5,b=3;
-		System.out.println(a+b);
+		int a =5,b=5;
+		System.out.println(b);
 	}
 
 }
